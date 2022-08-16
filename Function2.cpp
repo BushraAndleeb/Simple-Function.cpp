2b0df938097ae95e6a82a6bb32b7b792f75a4c9c
BushraAndleeb <110373190+BushraAndleeb@users.noreply.github.com>
2022-08-15 18:52:50 -0700

    Create Function2.cpp

---
+++ Function2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void Add(int a,int b);
+int main()
+{
+	Add(10,10);
+}
+void Add(int a,int b)
+{
+	int valueone=a;
+	int valuetwo=b;
+	cout<<valueone+valuetwo;
+		
+}
