1865214c24b4025c0792a072e6588be7235662cd
BushraAndleeb <110373190+BushraAndleeb@users.noreply.github.com>
2022-08-15 18:51:18 -0700

    Create Functions.cpp

---
+++ Functions.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void starline();
+int main()
+{
+	starline();
+	cout<<"Range of the data\n";
+	starline();
+	cout<<"I am Bushra Ali hashmi\n";
+	starline();
+}
+void starline()
+{
+	cout<<"************\n";
+}
