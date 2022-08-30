1ad9233c7b1319be796ebb00c93cfaae3bd5a0cf
BushraAndleeb <110373190+BushraAndleeb@users.noreply.github.com>
2022-08-29 23:02:31 -0700

    Create While loop.cpp

---
+++ While loop.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,j;
+	cout<<"Enter The number";
+	cin>>x>>j;
+	for(int k=1;k<=j;k++)
+	{
+		cout<<x<<"*"<<j<<"="<<x*k<<endl;
+	}
+	return 0;
+}
